30c1ac80e215eec0682eb3e45f068989c96850fb
saimyy <chshiev07@mail.ru>
2024-03-24 20:19:59 +0300

    partly ghome page

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', title='Home Page')
 
 
 if __name__ == '__main__':
