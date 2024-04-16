34ff94f3ef8eae1b68a4450308a7868c211e5725
saimyy <chshiev07@mail.ru>
2024-04-16 19:37:24 +0300

    30% project

--- bs4/test.py
+++ bs4/test.py
@@ -1,8 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
-session = requests.Session()
-k = session.post('https://codeforces.com/enter', {
-    'handleOrEmail': 'chshiev07@mail.ru',
-    'password': 'Slava2007@'})
-print(k)
+from selenium.webdriver.common.by import By
+
+handle = 'saimyy_'
+password = 'Slava2007@'
+driver = webdriver.Chrome()
+driver.get("https://codeforces.com/enter")
+email_input = driver.find_element(By.ID, 'handleOrEmail')
+email_input.clear()
+email_input.send_keys(handle)
+password_input = driver.find_element(By.ID, 'password')
+password_input.clear()
+password_input.send_keys(password)
+driver.find_element(By.CLASS_NAME, 'submit').click()
+driver.get("https://codeforces.com/group/Sm8APQqnoi/contests")
+time.sleep(5)
--- data/users.py
+++ data/users.py
@@ -19,8 +19,6 @@ class User(SqlAlchemyBase, UserMixin):
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
 
-    news = orm.relationship("News", back_populates='user')
-
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
