131426a39401473cd43cd6b429ce1fe5f31b7492
saimyy <chshiev07@mail.ru>
2024-04-17 20:31:04 +0300

    45%

--- bs4/test.py
+++ bs4/test.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
@@ -13,6 +14,11 @@ email_input.send_keys(handle)
 password_input = driver.find_element(By.ID, 'password')
 password_input.clear()
 password_input.send_keys(password)
+time.sleep(3)
 driver.find_element(By.CLASS_NAME, 'submit').click()
+time.sleep(5)
 driver.get("https://codeforces.com/group/Sm8APQqnoi/contests")
 time.sleep(5)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+k = soup.find_all('table')[5]
+print(k)
